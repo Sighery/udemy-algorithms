fd46fe26e500bbf6fe8be8894932cf02173899df
Sighery <sighery7@gmail.com>
2019-06-09 01:56:29 +0200

    Add package.json and entrypoint

---
+++ index.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+const ArrayStack = require("./arraystack.js").Stack;
+const DoublyLinkedList = require("./doublylinkedlist.js").DoublyLinkedList;
+const LinkedListStack = require("./linkedliststack.js").Stack;
+/* eslint-enable no-unused-vars */
