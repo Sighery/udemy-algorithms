e608a030f88119cdfafb53fd99bfe98b138722ab
Sighery <sighery7@gmail.com>
2019-06-09 23:26:16 +0200

    Add tests for enumerate util

---
+++ tests/test_utils.js
@@ -0,0 +1,23 @@
+import test from "ava";
+
+import {enumerate} from "../utils";
+
+
+function* dummyGenerator() {
+	const array = [1, 2, 3, 4];
+	for (let item of array) {
+		yield item;
+	}
+}
+
+
+test("Test enumerate with default arguments", t => {
+	const values = Array.from(enumerate(dummyGenerator()));
+	t.deepEqual(values, [[0, 1], [1, 2], [2, 3], [3, 4]]);
+});
+
+
+test("Test enumerate with negative start and step", t => {
+	const values = Array.from(enumerate(dummyGenerator(), -1, -1));
+	t.deepEqual(values, [[-1, 1], [-2, 2], [-3, 3], [-4, 4]]);
+});
