20c491ccbc3d5f3f98c19698fe65c53cd1581281
Sighery <sighery7@gmail.com>
2019-06-09 02:33:28 +0200

    Add Singly Linked List and SLL Queue to index

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
 const ArrayStack = require("./arraystack.js").Stack;
 const DoublyLinkedList = require("./doublylinkedlist.js").DoublyLinkedList;
+const LinkedListQueue = require("./linkedlistqueue.js").Queue;
 const LinkedListStack = require("./linkedliststack.js").Stack;
+const SinglyLinkedList = require("./singlylinkedlist.js").SinglyLinkedList;
 /* eslint-enable no-unused-vars */
